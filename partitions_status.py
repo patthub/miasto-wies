311996bc4a4d81196fe5c53f1efcd27ce38bf71b
Patryk Hubar <38289522+patthub@users.noreply.github.com>
2022-07-13 12:09:40 +0200

    Add files via upload

---
+++ partitions_status.py
@@ -0,0 +1,40 @@
+import geopandas as gpd
+import gdal
+from shapely.geometry import Point, Polygon
+from pyproj import Proj, transform
+import geopy.distance
+
+
+
+gdf_galicja = gpd.read_file("/home/patryk/Pulpit/miasto_wies/miasto-wies/galicja_mercator.shp")
+gdf_prusy = gpd.read_file("/home/patryk/Pulpit/miasto_wies/miasto-wies/prusy_mercator.shp")
+gdf_all = gpd.read_file("/home/patryk/Pulpit/miasto_wies/miasto-wies/zabory_all_mercator.shp")
+
+
+polygon_prusy = gdf_prusy['geometry'][0]
+polygon_galicja = gdf_galicja['geometry'][0]
+polygon_all = gdf_all['geometry'][0]
+
+
+def transform_pnt(longitude: float, latitude: float) -> Point:
+    raw_pnt = Point(longitude, latitude)
+    transformed_pnt = (transform(Proj(init='epsg:4326'), Proj(init='epsg:3857'), longitude, latitude))  # longitude first, latitude second.
+    return Point(transformed_pnt)
+
+def check_place_status(longitude: float, latitude: float) -> str:
+    pnt = transform_pnt(longitude, latitude)
+    if polygon_prusy.contains(pnt):
+        return "Zabór: Prusy"
+    elif polygon_galicja.contains(pnt):
+        return "Zabór: Galicja"
+    elif polygon_all.contains(pnt):
+        return "Zabór: Rosja"
+    else:
+        return "Miejsce poza zaborami Polski"
+
+
+
+
+print(check_place_status(20.233, 53.844))
+
+
