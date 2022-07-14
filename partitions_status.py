219339458a3698ad53a9e05781c5343d25788813
Patryk Hubar <38289522+patthub@users.noreply.github.com>
2022-07-14 15:35:17 +0200

    Update partitions_status.py
    added function for closest_distance

--- partitions_status.py
+++ partitions_status.py
@@ -38,7 +38,22 @@ def check_place_status(longitude: float, latitude: float) -> str:
         return "Miejsce poza zaborami Polski"
 
 
+from math import cos, asin, sqrt
 
+def distance(lat1, lon1, lat2, lon2):
+    p = 0.017453292519943295
+    hav = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p)*cos(lat2*p) * (1-cos((lon2-lon1)*p)) / 2
+    return 12742 * asin(sqrt(hav))
+
+def closest(data, v):
+    return min(data, key=lambda p: distance(v['lat'],v['lon'],p['lat'],p['lon']))
+
+tempDataList = [{'lat': 39.7612992, 'lon': -86.1519681}, 
+                {'lat': 39.762241,  'lon': -86.158436 }, 
+                {'lat': 39.7622292, 'lon': -86.1578917}]
+
+v = {'name': 'agnieszka', 'lat': 39.7622290, 'lon': -86.1519750}
+print(closest(tempDataList, v))
 
 
 #print(check_place_status(20.233, 53.844))
