73a417e56ba2bd28cc90b0763fed041146c5640c
Patryk Hubar <38289522+patthub@users.noreply.github.com>
2022-07-14 17:02:14 +0200

    Update partitions_status.py
    added mean

--- partitions_status.py
+++ partitions_status.py
@@ -37,6 +37,26 @@ def check_place_status(longitude: float, latitude: float) -> str:
     else:
         return "Miejsce poza zaborami Polski"
 
+    
+    
+    
+def get_mean_of_points(points: list) -> float:
+
+  sum_long = 0
+  sum_lat = 0
+  mean_long = 0
+  mean_lat = 0
+  length = len(points)
+
+  for elem in points:
+    sum_long += elem[0]
+    sum_lat += elem[1]
+    
+  mean_long = sum_long / length
+  mean_lat = sum_lat / length
+
+  return (mean_long, mean_lat)
+
 
 from math import cos, asin, sqrt
 
